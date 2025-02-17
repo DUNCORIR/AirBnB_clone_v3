d058172f3bd11f740fd315214c0fe17d62989bf5
duncorir <duncorir@mail.com>
2025-02-17 11:06:14 +0300

    not found

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 """Initialize Blueprint for API views"""
-
+# Import all view modules (PEP8 will complain, but it's required)
+from api.v1.views.index import *
 from flask import Blueprint
 
 # Create the app_views Blueprint with a URL prefix of /api/v1
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-# Import all view modules (PEP8 will complain, but it's required)
-from api.v1.views.index import *
